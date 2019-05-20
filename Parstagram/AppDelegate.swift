9c72e0e2cd978146b07226c52c25abf20dcc30c2
bokage <37666121+bokage@users.noreply.github.com>
2019-05-19 22:08:32 -0700

    Add files via upload

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -24,6 +24,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 configuration.server = "https://serene-springs-25324.herokuapp.com/parse"
             })
         )
+        if PFUser.current() != nil {
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
+            window?.rootViewController = feedNavigationController
+        }
+        
+        NSSetUncaughtExceptionHandler { exception in
+            print(exception)
+            print(exception.callStackSymbols)
+            
+        }
         
         return true
     }
@@ -49,7 +60,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
